09a92d1eef79ee719189ce2e416300dce35ba792
JonnyRobbie <marcodv@seznam.cz>
2016-06-09 21:26:14 +0200

    git prompt

--- links/.config/zsh-source/prompt.sh
+++ links/.config/zsh-source/prompt.sh
@@ -1,4 +1,7 @@
 #prompt
-autoload -U colors && colors
-PROMPT="┌ %{$fg_bold[white]%}%n%{$reset_color%}%{$fg_bold[red]%}@%{$reset_color%}%{$fg_bold[red]%}%m%{$reset_color%}%{$fg_bold[blue]%}(%*)%{$reset_color%} %{$fg_bold[green]%}%0~ %{$reset_color%}
-└ %(!.#.$) "
+autoload -U colors
+source ~/Github/zsh-git-prompt/zshrc.sh
+setopt promptsubst
+export ZSH_THEME_GIT_PROMPT_CACHE=1
+PROMPT='┌ %{$fg_bold[white]%}%n%{$reset_color%}%{$fg_bold[red]%}@%{$reset_color%}%{$fg_bold[red]%}%m%{$reset_color%}%{$fg_bold[blue]%}(%*)%{$reset_color%} %{$fg_bold[green]%}%0~ %{$reset_color%}
+└ $(git_super_status)%(!.#.$) '
