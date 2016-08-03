6f1f05d2e2621f60c58d9ff35b206b55e8a31d05
JonnyRobbie <marcodv@seznam.cz>
2016-08-03 13:06:28 +0200

    correct Czech tricolor

--- links/.config/zsh-source/prompt.sh
+++ links/.config/zsh-source/prompt.sh
@@ -3,5 +3,5 @@ autoload -U colors
 source ~/Github/zsh-git-prompt/zshrc.sh
 setopt promptsubst
 export ZSH_THEME_GIT_PROMPT_CACHE=1
-PROMPT='┌ %{$fg_bold[white]%}%n%{$reset_color%}%{$fg_bold[red]%}@%{$reset_color%}%{$fg_bold[red]%}%m%{$reset_color%}%{$fg_bold[blue]%}(%*)%{$reset_color%} %{$fg_bold[green]%}%0~ %{$reset_color%}
+PROMPT='┌ %{$fg_bold[white]%}%n%{$reset_color%}%{$fg_bold[blue]%}@%{$reset_color%}%{$fg_bold[blue]%}%m%{$reset_color%}%{$fg_bold[red]%}(%*)%{$reset_color%} %{$fg_bold[green]%}%0~ %{$reset_color%}
 └ $(git_super_status)%(!.#.$) '
