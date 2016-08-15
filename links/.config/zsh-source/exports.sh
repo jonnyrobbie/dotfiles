0e50b4c6b0d71efa79512fb95996741cb20a7832
JonnyRobbie <marcodv@seznam.cz>
2016-08-15 16:40:26 +0200

    ibus shenennigans

--- links/.config/zsh-source/exports.sh
+++ links/.config/zsh-source/exports.sh
@@ -1,8 +1,5 @@
 export WINEPREFIX=/data/Wine/win32
 export WINEARCH=win32
-export GTK_IM_MODULE=ibus
-export XMODIFIERS=@im=ibus
-export QT_IM_MODULE=ibus
 export PKGDEST=/var/cache/pacman/pkg-pacaur
 export AURDEST=/var/cache/aur/clone
 export SRCDEST=/var/cache/aur/source
