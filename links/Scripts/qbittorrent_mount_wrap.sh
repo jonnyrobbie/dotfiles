915bee090a062526dd7f22175b335d0b10d8b8b7
JonnyRobbie <marcodv@seznam.cz>
2016-10-26 22:39:03 +0200

    fix for always running bug

--- links/Scripts/qbittorrent_mount_wrap.sh
+++ links/Scripts/qbittorrent_mount_wrap.sh
@@ -3,7 +3,7 @@ uuid=DA1E35F71E35CCEF
 nomountname=/home/marek/.local/share/data/qBittorrent/BT_backup_nomount
 mountname=/home/marek/.local/share/data/qBittorrent/BT_backup_mount
 torrent_dir=/home/marek/.local/share/data/qBittorrent/BT_backup
-if pgrep qbittorrent > /dev/null; then
+if [ "$(pidof qbittorrent)" ]; then
     echo "qBittorrent is already running. Skipping resymlinking."
     notify-send "qBittorrent is already running" "Skipping resymlinking"
     echo "qbittorrent %U"
@@ -12,14 +12,16 @@ else
     if findmnt --source UUID=$uuid > /dev/null; then
         echo "Mount found"
         notify-send "My Book is mounted" "Linking to $mountname"
+		rm $torrent_dir
         ln -s $mountname $torrent_dir
         echo "qbittorrent %U"
-        qbittorrent $1
+        qbittorrent "$1"
     else
         echo "Mount not found"
         notify-send "My Book is not mounted" "Linking to $nomountname"
+		rm $torrent_dir
         ln -s $nomountname $torrent_dir
         echo "qbittorrent %U"
-        qbittorrent $1
+        qbittorrent "$1"
     fi
 fi
